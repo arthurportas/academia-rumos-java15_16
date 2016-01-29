7e668684fb8761232faec21621c46edf3d37654b
Arthur Portas <arthurportas@gmail.com>
2016-01-29 21:25:36 +0000

    Lab 6 solved

--- Holywood/src/main/java/com/example/holywood/ActorPosterCounter.java
+++ Holywood/src/main/java/com/example/holywood/ActorPosterCounter.java
@@ -6,30 +6,28 @@ import java.util.TreeMap;
 public class ActorPosterCounter {
 
 	
-	public ActorPosterCounter( Map map ){
-		
-	}
-	
-	public void processList( String[] arg ){
-		
-	}
-	
-	public void printReport(){
-		
-	}
-	public static void main( String[] args ) {
-		
+	public static void main(String[] args) {
+
 		// List of posters
-		String[] posters = new String[]{"1S01", "1S01", "1S01", "1S01", 
-				"1S01", "1S02", "1S02", "1S02", "1H01", "1H01", 
-				"1S02", "1S01", "1S01", "1H01", "1H01", "1H01", 
-				"1S02", "1S02", "1M02", "1M02", "1M02"};
-		// Create Actor Name Poster Number map
+		String[] posters = new String[] { "1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02", "1S02", "1H01",
+				"1H01", "1S02", "1S01", "1S01", "1H01", "1H01", "1H01", "1S02", "1S02", "1M02", "1M02", "1M02" };
+		// Create K=>Actor Name V=>Poster Number map
 		Map<String, String> posterActorNames = new TreeMap<>();
 		posterActorNames.put("Tom Hanks", "1S01");
-        posterActorNames.put("Brad Pitt", "1S02");
-        posterActorNames.put("Harrison Ford", "1H01");
-        posterActorNames.put("Jack Nicholson", "1M02");
+		posterActorNames.put("Brad Pitt", "1S02");
+		posterActorNames.put("Harrison Ford", "1H01");
+		posterActorNames.put("Jack Nicholson", "1M02");
 
+		ProductCounter pc1 = new ProductCounter( posterActorNames );
+		pc1.processList( posters );
+		pc1.printReport();
 	}
+
+	/*
+	 * DATASOURCE: posters impressos de cada ator
+	 * posterActorNames K,V - Ator e respetivo código sistema
+	 * Calcular o número de poster emitidos para cada ator
+	 * */
+
+	
 }
--- Holywood/src/main/java/com/example/holywood/ProductCounter.java
+++ Holywood/src/main/java/com/example/holywood/ProductCounter.java
@@ -6,14 +6,37 @@ import java.util.TreeMap;
 
 public class ProductCounter {
 
-	//The first map counts posters
+	// The first map counts posters
 	private Map<String, Long> postersCountMap = new HashMap<>();
-	//stores the mapping of actors to posters
+	// stores the mapping of actors to posters
 	private Map<String, String> posterActorNames = new TreeMap<>();
-	
-	public ProductCounter( Map posterActorNames ) {
+
+	public ProductCounter(Map posterActorNames) {
 		this.posterActorNames = posterActorNames;
 	}
 
-	
+	public void processList(String[] list) {
+		long curVal = 0;
+		for (String itemNumber : list) {
+			//postersCountMap {1S02=6, 1S01=7, 1M02=3, 1H01=5}
+			if (postersCountMap.containsKey(itemNumber)) {
+				curVal = postersCountMap.get(itemNumber);
+				curVal++;
+				postersCountMap.put(itemNumber, new Long(curVal));
+			} else {
+				postersCountMap.put(itemNumber, new Long(1));
+			}
+		}
+		System.out.println("");
+	}
+
+	public void printReport() {
+		System.out.println("=== Posters  Report ===");
+		//{Brad Pitt=1S02, Harrison Ford=1H01, Jack Nicholson=1M02, Tom Hanks=1S01}
+		for (String key : posterActorNames.keySet()) {
+			System.out.print("Actor Name: " + key);
+			System.out.println("\t\tCount: " + postersCountMap.get(posterActorNames.get(key)));
+		}
+	}
+
 }
